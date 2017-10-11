591de19626f98eca864b551cd65e709ce070ef9b
Буртылёв Сергей Николаевич <burtylev@intellex.ru>
2017-10-11 08:23:12 +0300

    removed the conflict

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -1,20 +1,11 @@
 import { combineReducers } from 'redux'
 import counterReducer from './counter'
 import articles from './articles'
-<<<<<<< HEAD
-import selectFilterReduser from './selectFilterReduser'
-import dateFilterReduser from './dateFilterReducer'
-export default combineReducers({
-    counter: counterReducer,
-    articles,
-    selected: selectFilterReduser,
-    dates: dateFilterReduser
-=======
 
 import filterReducer from './filterReducer'
 export default combineReducers({
     counter: counterReducer,
     articles,
     filter: filterReducer
->>>>>>> 43cd561fc18982d715d23ac2f90566940d080347
+
 })
